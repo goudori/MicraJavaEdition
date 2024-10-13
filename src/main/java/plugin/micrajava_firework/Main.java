c02c01b200663d9c1134e3d1c0a90181b5e6c0aa
go <144479783+goudori@users.noreply.github.com>
2024-10-13 13:21:30 +0900

    Update Main.java
    Update firework

--- src/main/java/plugin/micrajava_firework/Main.java
+++ src/main/java/plugin/micrajava_firework/Main.java
@@ -1,19 +1,29 @@
 package plugin.micrajava_firework;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
   @Override
   public void onEnable() {
     this.getCommand("firework").setExecutor(new FireworkCommand());
     getLogger().info("firework使用可能。");
-
+    Bukkit.getPluginManager().registerEvents(this, this);
   }
 
   @Override
   public void onDisable() {
     getLogger().info("fireworkは、使用できません。");
+  }
 
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    event.setJoinMessage(null);
   }
 }
